5834dfdf77b64cbe84dbe659a53191bb14d0e066
Sahil Wasan <sahilwasan000@gmail.com>
2019-04-07 14:04:02 +0530

    Update user.model.js

--- users/user.model.js
+++ users/user.model.js
@@ -6,7 +6,7 @@ const schema = new Schema({
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    createdDate: { type: Date, default: Date.now}
 });
 
 schema.set('toJSON', { virtuals: true });
